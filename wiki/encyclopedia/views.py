00c18865e6eb60021bd181a6e770c96a031231f1
lorcan quinn <lquinn3@tcd.ie>
2020-10-11 16:25:55 +0100

    attempt at taking user input in url and producing the relevant markup page for this input

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -2,9 +2,23 @@ from django.shortcuts import render
 
 from . import util
 
+import markdown
+
+
+#markdown.markdownFromFile( #converts a markdown input file into html
+#    input='input.md',
+#    output='output.html',
+#    encoding='utf8',
+#)
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry(request, name):
+    md_file = util.get_entry(f"entries/{name}.md")
+    markdown.markdownFromFile(input=md_file,
+        output='encyclopedia/templates/encyclopedia/converted.html',
+        encoding='utf8')
+    return render(request, "encyclopedia/converted.html" )
