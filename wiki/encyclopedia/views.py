7e1f4ce0e54525448f740e1f78a5959d03c05ccd
lorcan quinn <lquinn3@tcd.ie>
2020-10-11 21:09:11 +0100

    sends wiki/django etc to correct html page

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -4,23 +4,16 @@ from . import util
 
 import markdown
 
-
-#markdown.markdownFromFile( #converts a markdown input file into html
-#    input='input.md',
-#    output='output.html',
-#    encoding='utf8',
-#)
-
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+
 def entry(request, name):
-    md_file = util.get_entry(name) #this is setting md_file to the
-                                    #of the markdown file itself
-                                    #Need to save this string as a file instead
-    markdown.markdownFromFile(input=md_file,
-        output='encyclopedia/converted.html',
-        encoding='utf8')
-    return render(request, "encyclopedia/converted.html" )
+    file = open("encyclopedia/templates/encyclopedia/converted.html","w")
+    md = markdown.Markdown()
+    file.write(md.convert(util.get_entry(name)))
+    file.close()
+
+    return render(request, "encyclopedia/converted.html")
